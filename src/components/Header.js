9669ca674a0a19b2f2d76bc78d5bf58393beddad
latemiq <dzierzkowskij@gmail.com>
2023-03-15 02:18:51 +0100

    ADD: redux for cars

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,21 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import { selectCars } from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
 function Header(){ 
     const [burgerStatus, setBurgerStatus] = useState(false);
-
+    const cars = useSelector(selectCars)
+  
   return (
     <Container>
         <a>
             <img src="/images/logo.svg" alt="" />
         </a>
         <Menu>
+            {cars && cars.map((car, index)=>(
+                <a key={index} href="#">{car}</a>
+            ))}
             <a href="#">Model S</a>
             <a href="#">Model 3</a>
             <a href="#">Model X</a>
@@ -25,14 +31,16 @@ function Header(){
             <CloseWrapper>
             <CustomClose onClick={()=>setBurgerStatus(false)} />
             </CloseWrapper>
+
+            {cars && cars.map((car, index)=>(
+                <li key={index}><a href="#">{car}</a></li>
+            ))}
+
                     <li><a href="#">Existing Inventory</a></li>
                     <li><a href="#">Used Inventory</a></li>
                     <li><a href="#">Trade-in</a></li>
                     <li><a href="#">Cybertruck</a></li>
                     <li><a href="#">Roadster</a></li>
-                    <li><a href="#">Semi</a></li>
-                    <li><a href="#">Existing Inventory</a></li>
-                    <li><a href="#">Existing Inventory</a></li>
         </BurgerNav>
     </Container>
   )
@@ -94,6 +102,7 @@ display: flex;
 flex-direction: column;
 text-align: start;
 transform: ${props => props.show ? 'translateX(0)':'translateX(100%)'};
+transition: transform 0.2s;
 li{
     padding: 15px 0;
     border-bottom: 1px solid rgba(0, 0, 0, .2);
